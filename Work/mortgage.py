d4c046ab487bb68efd49c381a4ebf86230296dbf
TRGsci <trgcode@gmail.com>
2026-05-26 14:15:27 +0530

    completed mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,9 @@
-# mortgage.py
-#
-# Exercise 1.7
+principal = 500000
+rate = 0.05
+payment = 2684.11
+total_paid = 0
+
+while principal > 0:
+    principal = principal * (1 + rate/12) - payment
+    total_paid += payment
+print(f"Total paid: {total_paid:.2f}")
