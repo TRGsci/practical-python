c053be955ac8d093dfc5bae7cc1d55ca320ce27a
TRGsci <trgcode@gmail.com>
2026-05-29 09:58:15 +0530

    Solved exercise 1.9 --- Generalization of extra payments

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,19 +2,23 @@ principal = 500000
 rate = 0.05
 payment = 2684.11
 total_paid = 0
-months = 0
+months =0
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
-    if months < 12:
-        payment = 3684.11
+    if extra_payment_start_month <= months < extra_payment_end_month:
+        payment = 2684.11 + extra_payment
         principal = principal * (1 + rate/12) - payment
         total_paid += payment
         months += 1
-    elif months >= 12:
+    elif months < extra_payment_start_month or months >= extra_payment_end_month:
         payment = 2684.11
         principal = principal * (1 + rate/12) - payment
         total_paid += payment
         months += 1
+
 
 print(f"Total paid: {total_paid:.2f}")
 print(f"Months: {months}")
