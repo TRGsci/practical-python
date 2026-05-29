d958b4c696f6b8a80ee132e835bae546d3ff1220
TRGsci <trgcode@gmail.com>
2026-05-29 09:43:34 +0530

    Solved exercise 1.8 extra payments

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,8 +2,19 @@ principal = 500000
 rate = 0.05
 payment = 2684.11
 total_paid = 0
+months = 0
 
 while principal > 0:
-    principal = principal * (1 + rate/12) - payment
-    total_paid += payment
+    if months < 12:
+        payment = 3684.11
+        principal = principal * (1 + rate/12) - payment
+        total_paid += payment
+        months += 1
+    elif months >= 12:
+        payment = 2684.11
+        principal = principal * (1 + rate/12) - payment
+        total_paid += payment
+        months += 1
+
 print(f"Total paid: {total_paid:.2f}")
+print(f"Months: {months}")
