4b853b9037ca1e505956c39cfa499e96b5fdee6b
TRGsci <trgcode@gmail.com>
2026-05-29 10:07:55 +0530

    Solved exercise 1.10 ---  Making a table of payments

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,6 +1,6 @@
 principal = 500000
 rate = 0.05
-payment = 2684.11
+payment = 0
 total_paid = 0
 months =0
 extra_payment_start_month = 60
@@ -9,11 +9,13 @@ extra_payment = 1000
 
 while principal > 0:
     if extra_payment_start_month <= months < extra_payment_end_month:
+        print(f'Month: {months},Amount paid: {payment + extra_payment:.2f}, Remaining principal: {principal:.2f}')
         payment = 2684.11 + extra_payment
         principal = principal * (1 + rate/12) - payment
         total_paid += payment
         months += 1
     elif months < extra_payment_start_month or months >= extra_payment_end_month:
+        print(f'Month: {months},Amount paid: {payment:.2f}, Remaining principal: {principal:.2f}')
         payment = 2684.11
         principal = principal * (1 + rate/12) - payment
         total_paid += payment
