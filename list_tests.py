a591b7b1c7157e0c926b026fa2bf238898f592d8
TRGsci <trgcode@gmail.com>
2026-06-02 17:36:19 +0530

    Better naming for variables

--- list_tests.py
+++ list_tests.py
@@ -1,9 +1,9 @@
-symbols = 'HPQ,AAPL,IBM,MSFT,YHOO,DOA,GOOG'
-symlist = symbols.split(',')
-symlist.append('RHT')
-symlist.insert(1, 'AA')
-symlist.remove('MSFT')
-symlist.append('YHOO')
-x = ','.join(symlist)
-print(symlist)
+stockticker = 'HPQ,AAPL,IBM,MSFT,YHOO,DOA,GOOG'
+stklist = stockticker.split(',')
+stklist.append('RHT')
+stklist.insert(1, 'AA')
+stklist.remove('MSFT')
+stklist.append('YHOO')
+x = ','.join(stklist)
+print(stklist)
 print(x)
